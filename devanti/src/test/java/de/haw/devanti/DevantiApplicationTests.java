b10bd06c3767daae6c7de2619fbab9aa28e733e8
Narnvad <navidarmaniyar@gmail.com>
2024-12-10 12:39:51 +0100

    anfang

---
+++ devanti/src/test/java/de/haw/devanti/DevantiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.haw.devanti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevantiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
